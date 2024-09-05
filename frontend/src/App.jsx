bff81d4ed6e52d40dc5dc22ce7312daa35fd419c
LouisChristopher-001 <louischristopher784@gmail.com>
2024-09-05 15:18:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('employee-management-tt.vercel.app/register', {name, email, password})
+    axios.post('https://employee-management-tt.vercel.app/register', {name, email, password})
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
